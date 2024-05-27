d288aa24af96049a1c447d13e936a72e1fcd86d5
Joe mayinzi <joemayinzi@gmail.com>
2024-05-27 14:29:07 +0200

    inteigration : make the author page responsie

--- src/components/progress/ProgressBar.jsx
+++ src/components/progress/ProgressBar.jsx
@@ -3,19 +3,21 @@ import { useEffect, useState } from "react";
 
 export default function ProgressBar() {
   const [scrollY, setSrcollY] = useState(0);
-  const lastValueScrolledY = document.body.offsetHeight - window.innerHeight;
+  const [lastValue, setLastValue] = useState(0);
 
   useEffect(() => {
+    const lastValueScrolledY = document.body.offsetHeight - window.innerHeight;
+
+    setLastValue(lastValueScrolledY);
     document.addEventListener("scroll", () => {
       const scrollValue = window.scrollY;
       setSrcollY(scrollValue);
-      console.log(scrollY);
     });
   }, [scrollY]);
   return (
     <div className="relative">
       <progress
-        max={lastValueScrolledY}
+        max={lastValue}
         value={scrollY}
         className="w-full h-[6px] mb-3 fixed top-0 z-20"
       >
