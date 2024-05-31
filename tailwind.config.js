3cc25a47c2ceb5cfe779dfcfea52d10f8c697c73
Joe mayinzi <joemayinzi@gmail.com>
2024-05-31 23:04:53 +0200

    inteigration : make the author page responsie

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,6 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      screens: {
-        sm: "600px",
-        md: "728px",
-        lg: "984px",
-        xl: "1200px",
-        "2xl": "1496px",
-      },
     },
 
     extend: {
@@ -25,5 +18,18 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          "@screen lg": {
+            maxWidth: "1200px",
+          },
+          "@screen xl": {
+            maxWidth: "1200px",
+          },
+        },
+      });
+    },
+  ],
 };
